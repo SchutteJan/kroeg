099bb547df25da07caad8240ee292b306cb93a43
Jan Schutte <jan.o.schutte@gmail.com>
2023-07-05 22:33:17 +0200

    Add loading indicator to homepage

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,32 +1,37 @@
 <script lang="ts">
-  import BarItem from "../lib/BarItem.svelte";
+  import BarItem from "../lib/BarItem.svelte"
 
-  let bars = [];
+  let bars = []
+  let loading = true
   
   fetch('http://localhost:8000/bars', {
     method: 'GET',
-  }).then(response => response.json())
-    .then(json => { bars = json });
+  }).then(response => {
+    loading = false
+    return response.json()
+  }).then(json => { bars = json });
   
 </script>
 
 <section>
 <header class="container">
     <hgroup>
-        <h1>All bars</h1>
+        <h1>Kroegen</h1>
         <p>
-        Below is a list of all bars in Amsterdam
+        Zie hier een lijst van alle kroegen in Amsterdam.
         </p>
     </hgroup>
 </header>
-<p>
-  Here is where that content will live..
-</p>
 
-<ul>
-  {#each bars as bar, i}
-    <BarItem name={bar.name} description={bar.description}/>
-  {/each}
-</ul>
+
+<div class="bar-list-container">
+  {#if loading}
+    <progress/>
+  {:else}
+    {#each bars as bar, i}
+      <BarItem name={bar.name} description={bar.description}/>
+    {/each}
+  {/if}
+</div>
 
 </section>

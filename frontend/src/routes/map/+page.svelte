830b48a763536f5a65dc4f91c3b23d34c57a08be
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2024-09-14 11:56:01 +0200

    feat: add live location marker

--- frontend/src/routes/map/+page.svelte
+++ frontend/src/routes/map/+page.svelte
@@ -5,10 +5,13 @@
 	import { get_areas } from '../../api/areas'
 	import type { Area, LocationResponse } from '../../models/schemas'
 	import MapBarItem from '$lib/MapBarItem.svelte'
+	import type { LocationEvent, Map, Circle } from 'leaflet'
 
 	let mapElement: HTMLElement
+	let liveLocationMarker = null
 	const GREEN = '#00895A'
 	const RED = '#D93526'
+	const BLUE = '#0172AD' // --pico-primary
 
 	function renderPopup(layer: any, bar: LocationResponse) {
 		let container = document.createElement('div')
@@ -29,7 +32,7 @@
 		return container
 	}
 
-	function addBarMapAnnotations(L: any, map: any, bars: Array<LocationResponse>) {
+	function addBarMapAnnotations(L: any, map: Map, bars: Array<LocationResponse>) {
 		bars.forEach((bar) => {
 			L.circle([bar.coordinates.x, bar.coordinates.y], {
 				color: bar.visited_at ? GREEN : RED,
@@ -44,12 +47,11 @@
 		})
 	}
 
-	function addAreaAnnotations(L: any, map: any, areas: Array<Area>) {
+	function addAreaAnnotations(L: any, map: Map, areas: Array<Area>) {
 		areas.forEach((area) => {
 			L.polygon(
 				area.area.rings.map((ring) => ring.map((point) => [point.x, point.y])),
-				// #0172ad is equivalent to --pico-primary
-				{ color: '#0172ad', fill: false }
+				{ color: BLUE, fill: false }
 			)
 				.bindPopup(area.name)
 				.addTo(map)
@@ -61,6 +63,25 @@
 
 		// Amsterdam center
 		const map = L.map(mapElement).setView([52.3694028, 4.9012861], 13)
+
+		let locationMarkerDot: Circle | null = null
+		let locationmarkerRadius: Circle | null = null
+		// Live location
+		function onLocationFound(e: LocationEvent) {
+			if (locationMarkerDot) {
+				map.removeLayer(locationMarkerDot)
+			}
+			if (locationmarkerRadius) {
+				map.removeLayer(locationmarkerRadius)
+			}
+			locationMarkerDot = L.circle(e.latlng, { radius: 3, color: BLUE, fillOpacity: 1 }).addTo(map)
+			locationmarkerRadius = L.circle(e.latlng, {
+				radius: e.accuracy,
+				color: BLUE,
+				fillOpacity: 0.1
+			}).addTo(map)
+		}
+		map.on('locationfound', onLocationFound)
 
 		// https://api.data.amsterdam.nl/api/
 		L.tileLayer('https://t2.data.amsterdam.nl/topo_wm_light/{z}/{x}/{y}.png', {
@@ -81,6 +102,27 @@
 			.then((bars) => {
 				addBarMapAnnotations(L, map, bars)
 			})
+
+		// Add locate button
+		let LocateControl = L.Control.extend({
+			onAdd: function (map: Map) {
+				let button = L.DomUtil.create('button')
+				button.innerHTML = '🎯'
+				button.style.border = 'none'
+				button.style.backgroundColor = 'white'
+				button.style.fontSize = '20px'
+				button.style.cursor = 'pointer'
+				button.style.margin = '10px'
+				button.style.padding = '0.5rem 0.7rem'
+				button.style.boxShadow = '0 0 5px rgba(0,0,0,0.3)'
+				button.onclick = function () {
+					map.locate({ setView: true, maxZoom: 16 })
+				}
+				return button
+			}
+		})
+
+		new LocateControl({ position: 'topright' }).addTo(map)
 	})
 </script>
 
